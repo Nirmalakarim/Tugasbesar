dc6759ecb08ddcddcd8c6b726fe0f066f9d64420
NirmalaKarim <NirmalaKarim@toshiba>
2015-04-24 16:31:26 +0700

    new

---
+++ src/Ruangkelas.java
@@ -0,0 +1,173 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author NirmalaKarim
+ */
+public class Ruangkelas {
+        private String lantai;
+    private String dinding;
+    private String atap;
+    private String pintu;
+    private String jendela;
+    private String sirkulasi_udara;
+    private String pencayahan;
+    private String kelembapan;
+    private String suhu;
+    private String kebisingan;
+    private String bau;
+    private String kebocoran;
+    private String keausan;
+    private String kerusakan;
+    private String kekokohan;
+    private String kunci_pintu;
+    private String kunci_jendela;
+    private String bahaya;
+
+    public String getLantai() {
+        return lantai;
+    }
+
+    public void setLantai(String lantai) {
+        this.lantai = lantai;
+    }
+
+    public String getDinding() {
+        return dinding;
+    }
+
+    public void setDinding(String dinding) {
+        this.dinding = dinding;
+    }
+
+    public String getAtap() {
+        return atap;
+    }
+
+    public void setAtap(String atap) {
+        this.atap = atap;
+    }
+
+    public String getPintu() {
+        return pintu;
+    }
+
+    public void setPintu(String pintu) {
+        this.pintu = pintu;
+    }
+
+    public String getJendela() {
+        return jendela;
+    }
+
+    public void setJendela(String jendela) {
+        this.jendela = jendela;
+    }
+
+    public String getSirkulasi_udara() {
+        return sirkulasi_udara;
+    }
+
+    public void setSirkulasi_udara(String sirkulasi_udara) {
+        this.sirkulasi_udara = sirkulasi_udara;
+    }
+
+    public String getPencayahan() {
+        return pencayahan;
+    }
+
+    public void setPencayahan(String pencayahan) {
+        this.pencayahan = pencayahan;
+    }
+
+    public String getKelembapan() {
+        return kelembapan;
+    }
+
+    public void setKelembapan(String kelembapan) {
+        this.kelembapan = kelembapan;
+    }
+
+    public String getSuhu() {
+        return suhu;
+    }
+
+    public void setSuhu(String suhu) {
+        this.suhu = suhu;
+    }
+
+    public String getKebisingan() {
+        return kebisingan;
+    }
+
+    public void setKebisingan(String kebisingan) {
+        this.kebisingan = kebisingan;
+    }
+
+    public String getBau() {
+        return bau;
+    }
+
+    public void setBau(String bau) {
+        this.bau = bau;
+    }
+
+    public String getKebocoran() {
+        return kebocoran;
+    }
+
+    public void setKebocoran(String kebocoran) {
+        this.kebocoran = kebocoran;
+    }
+
+    public String getKeausan() {
+        return keausan;
+    }
+
+    public void setKeausan(String keausan) {
+        this.keausan = keausan;
+    }
+
+    public String getKerusakan() {
+        return kerusakan;
+    }
+
+    public void setKerusakan(String kerusakan) {
+        this.kerusakan = kerusakan;
+    }
+
+    public String getKekokohan() {
+        return kekokohan;
+    }
+
+    public void setKekokohan(String kekokohan) {
+        this.kekokohan = kekokohan;
+    }
+
+    public String getKunci_pintu() {
+        return kunci_pintu;
+    }
+
+    public void setKunci_pintu(String kunci_pintu) {
+        this.kunci_pintu = kunci_pintu;
+    }
+
+    public String getKunci_jendela() {
+        return kunci_jendela;
+    }
+
+    public void setKunci_jendela(String kunci_jendela) {
+        this.kunci_jendela = kunci_jendela;
+    }
+
+    public String getBahaya() {
+        return bahaya;
+    }
+
+    public void setBahaya(String bahaya) {
+        this.bahaya = bahaya;
+    }
+}
