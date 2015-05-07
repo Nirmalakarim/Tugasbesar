0190bf64e0084b8cce4df5086baec89ae4150fd2
NirmalaKarim <NirmalaKarim@toshiba>
2015-05-08 01:05:47 +0700

    Deklarasi ruangkelas

--- src/Deklarasi_ruangkelas.java
+++ src/Deklarasi_ruangkelas.java
@@ -10,47 +10,48 @@ import java.util.Scanner;
  *
  * @author asus cb46c
  */
-public class Deklarasi_ruangkelas {
+public class Deklarasi_ruangkelas extends Ruangkelas{
     Scanner scan = new Scanner(System.in);
     void identitas_ruang(){
-        Ruangkelas kelas = new Ruangkelas();
         System.out.println("Selamat datang");
         System.out.print("Masukan nama ruang :");
-        kelas.setNama_ruang(scan.next());
+        super.setNama_ruang(scan.next());
         System.out.print("Masukan lokasi ruang :");
-        kelas.setLokasi_ruang(scan.next());
+        super.setLokasi_ruang(scan.next());
         System.out.print("Masukan Jurusan anda :");
-        kelas.setProdi(scan.next());
+        super.setProdi(scan.next());
         
-        System.out.println("==================");
-        System.out.println("");
-        System.out.println("Nama ruang : "+kelas.getNama_ruang());
-        System.out.println("Lokasi Ruang : "+kelas.getLokasi_ruang());
-        System.out.println("Jurusan : "+kelas.getProdi());
+        System.out.println("================== \n");
+        System.out.println("Nama ruang : "+super.getNama_ruang());
+        System.out.println("Lokasi Ruang : "+super.getLokasi_ruang());
+        System.out.println("Jurusan : "+super.getProdi());
     }
     void kondisi_ruangkelas(){
-        Ruangkelas kelas = new Ruangkelas();
-        System.out.println("*kondisi ruang kelas*");
-        System.out.print("Masukan panjang ruang : ");
-        kelas.setPanjang_ruang(scan.nextInt());
-        System.out.print("Masukan lebar ruang : ");
-        kelas.setLebar_ruang(scan.nextInt());
-        System.out.println("Luas : "+kelas.luas());
-        System.out.print("Bentuk : ");
-        kelas.bentuk();
-        System.out.println("");
-        System.out.print("Masukan jumlah kursi : ");
-        kelas.setJumlah_kursi(scan.nextInt());
-        System.out.println("Rasio : "+kelas.rasio());
-        if (kelas.rasio()>=0.5){
-            System.out.println("sesuai");
-        }else {
-            System.out.println("tidak sesuai");
-        }
-        System.out.print("Masukan jumlah pintu : ");
-        kelas.setJumlah_pintu(scan.nextInt());
-        System.out.print("Masukan jumlah jendela : ");
-        kelas.setJumlah_jendela(scan.nextInt());
-        kelas.analisis();
+        System.out.println("\nTahap kondisi Ruang kelas\n");
+        System.out.println("A. Masukan panjang ruang ");
+        System.out.print("Jawab :");
+        super.setPanjang_ruang(scan.nextInt());
+        System.out.println("B. Masukan lebar ruang ");
+        System.out.print("Jawab :");
+        super.setLebar_ruang(scan.nextInt());
+        System.out.println("C. Luas : "+luas());
+        System.out.println("D. Bentuk : "+bentuk());
+        System.out.println("E. Masukan jumlah kursi");
+        System.out.print("Jawab :");
+        super.setJumlah_kursi(scan.nextInt());
+        System.out.println("F. Rasio : "+rasio());
+        System.out.println("Rasio : "+Analisis_rasio());
+        System.out.println("G. Masukan jumlah pintu ");
+        System.out.print("Jawab :");
+        super.setJumlah_pintu(scan.nextInt());
+        System.out.println("H. Masukan jumlah jendela");
+        System.out.print("Jawab : ");
+        super.setJumlah_jendela(scan.nextInt());
+        System.out.println("Jumlah pintu dan jendela :"+analisisPintuDanJendela());
+    }
+    public static void main(String[] args) {
+        Deklarasi_ruangkelas n = new Deklarasi_ruangkelas();
+        n.identitas_ruang();
+        n.kondisi_ruangkelas();
     }
 }
