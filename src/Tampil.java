34f2f139a17bf872124d8ea2fe8e1cd0c49934db
NirmalaKarim <NirmalaKarim@toshiba>
2015-05-18 00:30:57 +0700

    Untuk menampilkan

---
+++ src/Tampil.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author NirmalaKarim
+ */
+public class Tampil {
+    public static void main(String[] args) {
+        Deklarasi_Lingkungan test1 = new Deklarasi_Lingkungan();
+        Deklarasi_Kenyamanan test2 = new Deklarasi_Kenyamanan();
+        Deklarasi_kondisi test3 = new Deklarasi_kondisi(); 
+        Deklarasi_Fasilitas test4 = new Deklarasi_Fasilitas();
+        test3.deklarasi();
+        test4.deklarasi();
+        test1.deklarasi();
+        test2.deklarasi();
+    }
+}
