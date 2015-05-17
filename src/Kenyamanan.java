c6d58dcc9caf6513fec9bd761ebeba45ef421034
NirmalaKarim <NirmalaKarim@toshiba>
2015-05-18 00:32:13 +0700

    Deklarasi Kenyamanan

--- src/Kenyamanan.java
+++ src/Kenyamanan.java
@@ -1,6 +1,4 @@
 
-import java.util.Scanner;
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -10,113 +8,128 @@ import java.util.Scanner;
  *
  * @author NirmalaKarim
  */
-public class Kenyamanan {
-    Scanner scan = new Scanner(System.in);
+public class Kenyamanan extends Ruangkelas{
+    int pilih_kebisingan;
+    int pilih_bau;
+    int pilih_kebocoran;
+    int pilih_kerusakan;
+    int pilih_keausan;
+    int pilih_kekokohan;
+    int pilih_kuncipintu;
+    int pilih_kuncijendela;
+    int pilih_bahaya;
 
-    private int kebisingan;
-    private int bau;
-    private int kebocoran;
-    private int keausan;
-    private int kerusakan;
-    private int kekokohan;
-    private int kunci_pintu;
-    private int kunci_jendela;
-    private int bahaya;
-
-    public int getKebisingan() {
-        return kebisingan;
+    public Kenyamanan(String kondisi) {
+        super(kondisi);
     }
 
-    public void setKebisingan(int kebisingan) {
-        this.kebisingan = kebisingan;
+    //override
+    @Override
+    public void setKondisiKenyamanan(int kebisingan, int bau, int kebocoran, int kerusakan, int keausan) {
+        super.setKondisiKenyamanan(kebisingan, bau, kebocoran, kerusakan, keausan);
     }
 
-    public int getBau() {
-        return bau;
+    @Override
+    public void setKondisiKenyamanan(int kekokohan, int kunci_pintu, int kunci_jendela, int bahaya) {
+        super.setKondisiKenyamanan(kekokohan, kunci_pintu, kunci_jendela, bahaya);
+    }
+    
+    
+    //enkapsulasi
+    public int getPilih_kebisingan() {
+        return pilih_kebisingan;
     }
 
-    public void setBau(int bau) {
-        this.bau = bau;
+    public void setPilih_kebisingan(int pilih_kebisingan) {
+        this.pilih_kebisingan = pilih_kebisingan;
     }
 
-    public int getKebocoran() {
-        return kebocoran;
+    public int getPilih_bau() {
+        return pilih_bau;
     }
 
-    public void setKebocoran(int kebocoran) {
-        this.kebocoran = kebocoran;
+    public void setPilih_bau(int pilih_bau) {
+        this.pilih_bau = pilih_bau;
     }
 
-    public int getKeausan() {
-        return keausan;
+    public int getPilih_kebocoran() {
+        return pilih_kebocoran;
     }
 
-    public void setKeausan(int keausan) {
-        this.keausan = keausan;
+    public void setPilih_kebocoran(int pilih_kebocoran) {
+        this.pilih_kebocoran = pilih_kebocoran;
     }
 
-    public int getKerusakan() {
-        return kerusakan;
+    public int getPilih_kerusakan() {
+        return pilih_kerusakan;
     }
 
-    public void setKerusakan(int kerusakan) {
-        this.kerusakan = kerusakan;
+    public void setPilih_kerusakan(int pilih_kerusakan) {
+        this.pilih_kerusakan = pilih_kerusakan;
     }
 
-    public int getKekokohan() {
-        return kekokohan;
+    public int getPilih_keausan() {
+        return pilih_keausan;
     }
 
-    public void setKekokohan(int kekokohan) {
-        this.kekokohan = kekokohan;
+    public void setPilih_keausan(int pilih_keausan) {
+        this.pilih_keausan = pilih_keausan;
     }
 
-    public int getKunci_pintu() {
-        return kunci_pintu;
+    public int getPilih_kekokohan() {
+        return pilih_kekokohan;
     }
 
-    public void setKunci_pintu(int kunci_pintu) {
-        this.kunci_pintu = kunci_pintu;
+    public void setPilih_kekokohan(int pilih_kekokohan) {
+        this.pilih_kekokohan = pilih_kekokohan;
     }
 
-    public int getKunci_jendela() {
-        return kunci_jendela;
+    public int getPilih_kuncipintu() {
+        return pilih_kuncipintu;
     }
 
-    public void setKunci_jendela(int kunci_jendela) {
-        this.kunci_jendela = kunci_jendela;
+    public void setPilih_kuncipintu(int pilih_kuncipintu) {
+        this.pilih_kuncipintu = pilih_kuncipintu;
     }
 
-    public int getBahaya() {
-        return bahaya;
+    public int getPilih_kuncijendela() {
+        return pilih_kuncijendela;
     }
 
-    public void setBahaya(int bahaya) {
-        this.bahaya = bahaya;
+    public void setPilih_kuncijendela(int pilih_kuncijendela) {
+        this.pilih_kuncijendela = pilih_kuncijendela;
     }
-      
-    String kebisingan(){
+
+    public int getPilih_bahaya() {
+        return pilih_bahaya;
+    }
+
+    public void setPilih_bahaya(int pilih_bahaya) {
+        this.pilih_bahaya = pilih_bahaya;
+    }
+    
+    //untuk mengembalikan nilai
+   String kebisingan(){
         String pilih;
-        if(getKebisingan()==2){
+        if(getPilih_kebisingan()==kebisingan){
             pilih = "sessuai";
         }else{
             pilih = "tidak sesuai";
         }
-        return pilih;        
+        return pilih;
     }
     String bau(){
         String pilih;
-        if(getBau()==2){
+        if(getPilih_bau()==bau){
             pilih="sesuai";
         }else{
             pilih = "tidak sesuai";
         }
         return pilih;
-        
     }
-    String kebocoran (){
+    String kebocoran(){
         String pilih;
-        if(getKebocoran()==2){
+        if(getPilih_kebocoran()==kebocoran){
             pilih="sesuai";
         } else{
             pilih="tidak sesuai";
@@ -125,16 +138,16 @@ public class Kenyamanan {
     }
     String keausan(){
         String pilih;
-        if(getKeausan()==2){
+        if(getPilih_keausan()==keausan){
             pilih="sesuai";
         } else{
             pilih="tidak sesuai";
         }
         return pilih;
-    }
+    }    
     String kerusakan(){
         String pilih;
-        if(getKerusakan()==2){
+        if(getPilih_kerusakan()==kerusakan){
             pilih="sesuai";
         } else{
             pilih="tidak sesuai";
@@ -143,7 +156,7 @@ public class Kenyamanan {
     }
     String kekokohan(){
         String pilih;
-        if(getKekokohan()==1){
+        if(getPilih_kekokohan()==kekokohan){
             pilih="sesuai";
         } else{
             pilih="tidak sesuai";
@@ -152,16 +165,16 @@ public class Kenyamanan {
     }
     String kunci(){
         String pilih;
-        if(getKunci_pintu()==1 && getKunci_jendela()==1){
+        if(getPilih_kuncipintu()==kunci_pintu && getPilih_kuncijendela()==kunci_jendela){
             pilih="sesuai";
         } else{
             pilih="tidak sesuai";
         }
         return pilih;
     }
-    String keamanan(){
+    String keamanan (){
         String pilih;
-        if(getBahaya()==1){
+        if(getPilih_bahaya()==bahaya){
             pilih="sesuai";
         } else{
             pilih="tidak sesuai";
