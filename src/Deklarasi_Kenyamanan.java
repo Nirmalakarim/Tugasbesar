b16f8a166470ce5e0d8a0a1739d32aa2453dcbed
Dini Saputri Yanianti <diniisaputrii@gmail.com>
2015-05-25 09:04:18 +0700

    Update Deklarasi_Kenyamanan.java

--- src/Deklarasi_Kenyamanan.java
+++ src/Deklarasi_Kenyamanan.java
@@ -19,35 +19,35 @@ public class Deklarasi_Kenyamanan{
     public void deklarasi_kenyamanan(Kenyamanan test) {
         test.setKondisiKenyamanan(2, 2, 2, 2, 2);
         System.out.println(test.kondisi);
-        System.out.println("A. Apakah di kelas bising");
+        System.out.println("A. Apakah di kelas bising ?");
         System.out.println("1. Bising");
         System.out.println("2. Tidak Bising");
         System.out.print("Jawab : ");
         test.setPilih_kebisingan(scan.nextInt());
         System.out.println("Kebisingan :"+test.kebisingan());
         
-        System.out.println("B. Apakah di kelas bau");
+        System.out.println("B. Apakah di kelas bau ?");
         System.out.println("1. Bau");
         System.out.println("2. Tidak Bau");
         System.out.print("Jawab : ");
         test.setPilih_bau(scan.nextInt());
         System.out.println("Bau :"+test.bau());
                
-        System.out.println("C. Apakah di kelas ada yang bocor");
+        System.out.println("C. Apakah di kelas ada yang bocor ?");
         System.out.println("1. Ada ");
         System.out.println("2. Tidak Ada");
         System.out.print("Jawab : ");
         test.setPilih_kebocoran(scan.nextInt());
         System.out.println("Kebocoran :"+test.kebocoran());
         
-        System.out.println("D. Apakah di kelas ada aus");
+        System.out.println("D. Apakah di kelas ada aus ?");
         System.out.println("1. Ada ");
         System.out.println("2. Tidak Ada");
         System.out.print("Jawab : ");
         test.setPilih_keausan(scan.nextInt());
         System.out.println("Keausan :"+test.keausan());
         
-        System.out.println("E. Apakah di kelas ada yang rusak");
+        System.out.println("E. Apakah di kelas ada yang rusak ?");
         System.out.println("1. Ada ");
         System.out.println("2. Tidak Ada ");
         System.out.print("Jawab : ");
@@ -57,27 +57,27 @@ public class Deklarasi_Kenyamanan{
     public void deklarasi_keamanan(Kenyamanan test) {
         test.setKondisiKenyamanan(1, 1, 1, 1);
         System.out.println(test.kondisi);
-        System.out.println("A. Apakah kelas masih kokoh");
+        System.out.println("A. Apakah kelas masih kokoh ?");
         System.out.println("1. Masih Kokoh");
         System.out.println("2. Tidak Kokoh");
         System.out.print("Jawab : ");
         test.setPilih_kekokohan(scan.nextInt());
         System.out.println("Kekokohan :"+test.kekokohan());
         
-        System.out.println("B. Bagaimana keaadaan kunci pintu");
+        System.out.println("B. Bagaimana keaadaan kunci pintu ?");
         System.out.println("1. Ada");
         System.out.println("2. Tidak ada");
         System.out.print("Jawab : ");
         test.setPilih_kuncipintu(scan.nextInt());
         
-        System.out.println("C. Bagaimana keaadaan kunci jendela");
+        System.out.println("C. Bagaimana keaadaan kunci jendela ?");
         System.out.println("1. Ada");
         System.out.println("2. Tidak ada");
         System.out.print("Jawab : ");
         test.setPilih_kuncijendela(scan.nextInt());
         System.out.println("Analisis kunci pintu dan jendela :"+test.kunci());
         
-        System.out.println("D. Apakah di kelas ada Bahaya");
+        System.out.println("D. Apakah di kelas ada Bahaya ?");
         System.out.println("1. Aman");
         System.out.println("2. Tidak Aman");
         System.out.print("Jawab : ");
