c324469b7fc469dc19faf678dfb33ac22661a116
NirmalaKarim <NirmalaKarim@toshiba>
2015-05-26 18:04:07 +0700

    Abstrak & interface deklarasi kondisi

--- src/Deklarasi_kondisi.java
+++ src/Deklarasi_kondisi.java
@@ -1,17 +1,13 @@
-
-import java.util.Scanner;
-
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 /**
  *
- * @author asus cb46c
+ * @author NirmalaKarim
  */
-public class Deklarasi_kondisi{
-    Scanner scan = new Scanner(System.in);
+public class Deklarasi_kondisi {
     void deklarasi(){
         identitas (new Identitas_kondisi("Tahap Identitas Kelas"));
         kondisi (new Identitas_kondisi("Tahap Kondisi Kelas"));
@@ -21,26 +17,8 @@ public class Deklarasi_kondisi{
        
     }
     public void kondisi(Identitas_kondisi test) {
-        System.out.println(test.kondisi);
-        System.out.println("A. Masukan panjang ruang ? ");
-        System.out.print("Jawab :");
-        test.setPanjang_ruang(scan.nextInt());
-        System.out.println("B. Masukan lebar ruang ?");
-        System.out.print("Jawab :");
-        test.setLebar_ruang(scan.nextInt());
-        System.out.println("C. Luas : "+test.luas());
-        System.out.println("D. Bentuk : "+test.bentuk());
-        System.out.println("E. Masukan jumlah kursi ?");
-        System.out.print("Jawab :");
-        test.setJumlah_kursi(scan.nextInt());
-        System.out.println("F. Rasio : "+test.rasio());
-        System.out.println("Rasio : "+test.Analisis_rasio());
-        System.out.println("G. Masukan jumlah pintu  ?");
-        System.out.print("Jawab :");
-        test.setJumlah_pintu(scan.nextInt());
-        System.out.println("H. Masukan jumlah jendela ?");
-        System.out.print("Jawab : ");
-        test.setJumlah_jendela(scan.nextInt());
-        System.out.println("Jumlah pintu dan jendela :"+test.analisisPintuDanJendela());
+        test.deklarasi2();
     }
+
+    
 }
