121b5862da5387e386c6d87eddac64301303362b
Dini Saputri Yanianti <diniisaputrii@gmail.com>
2015-06-05 19:56:53 +0700

    deklarasi kenyamanan (polimorfisme)

---
+++ src/Deklarasi_Kenyamanan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author NirmalaKarim
+ */
+public class Deklarasi_Kenyamanan {
+    Scanner scan = new Scanner(System.in);
+    void deklarasi(){
+        deklarasi_kenyamanan(new Kenyamanan("Tahap Kondisi Kenyamanan"));
+        deklarasi_keamanan(new Kenyamanan("Tahap Kondisi Keamanan"));      
+    }
+    public void deklarasi_kenyamanan(Kenyamanan test) {
+        test.deklarasi1();
+    }
+    public void deklarasi_keamanan(Kenyamanan test) {
+        test.deklarasi2();
+    }
+}
