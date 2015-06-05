71ed827f43c118196a3a0d4d5dce52bd078a4c5d
Dini Saputri Yanianti <diniisaputrii@gmail.com>
2015-06-05 19:51:38 +0700

    Ruangkelas (induk+abstrak class)

--- src/Ruangkelas.java
+++ src/Ruangkelas.java
@@ -3,118 +3,280 @@
  * and open the template in the editor.
  */
 
-/**
- *
- * @author NirmalaKarim
- */
-public class Ruangkelas {
-    private String Nama_ruang;
-    private String Lokasi_ruang;
-    private String Prodi;
-    private int Panjang_ruang;
-    private int Lebar_ruang;
-    private int Jumlah_kursi;
-    private int Jumlah_meja;
-    private int Jumlah_jendela;
-    private int jumlah_pintu;
+import java.util.Scanner;
 
-    public String getNama_ruang() {
-        return Nama_ruang;
+abstract public class Ruangkelas{
+    Scanner scan = new Scanner(System.in);
+    
+    private String Username;
+    private String pasword;
+    private int kondisisteker;
+    private int kondisikabelLCD;
+    private int posisikabelLCD;
+    private int kondisilampu;
+    private int posisilampu;
+    private int kondisikipasangin;
+    private int posisikipasangin;
+    private int kondisiAC;
+    private int posisiAC;
+    private int bandwidth;
+    private int kondisiCCTV;
+    private int kebisingan;
+    private int bau;
+    private int kebocoran;
+    private int keausan;
+    private int kerusakan;
+    private int kekokohan;
+    private int kunci_pintu;
+    private int kunci_jendela;
+    private int bahaya;
+    private int lantai;
+    private int dinding;
+    private int atap;
+    private int pintu;
+    private int jendela;
+    private int sirkulasi_udara;
+    private String kondisi;
+    int a;
+    int b;
+    
+    public Ruangkelas(String kondisi) {//Konstraktor
+        this.kondisi = kondisi;
     }
 
-    public void setNama_ruang(String Nama_ruang) {
-        this.Nama_ruang = Nama_ruang;
-    }
-
-    public String getLokasi_ruang() {
-        return Lokasi_ruang;
-    }
-
-    public void setLokasi_ruang(String Lokasi_ruang) {
-        this.Lokasi_ruang = Lokasi_ruang;
-    }
-
-    public String getProdi() {
-        return Prodi;
-    }
-
-    public void setProdi(String Prodi) {
-        this.Prodi = Prodi;
-    }
-
-    public int getPanjang_ruang() {
-        return Panjang_ruang;
-    }
-
-    public void setPanjang_ruang(int Panjang_ruang) {
-        this.Panjang_ruang = Panjang_ruang;
-    }
-
-    public int getLebar_ruang() {
-        return Lebar_ruang;
-    }
-
-    public void setLebar_ruang(int Lebar_ruang) {
-        this.Lebar_ruang = Lebar_ruang;
-    }
-
-    public int getJumlah_kursi() {
-        return Jumlah_kursi;
-    }
-
-    public void setJumlah_kursi(int Jumlah_kursi) {
-        this.Jumlah_kursi = Jumlah_kursi;
-    }
-
-    public int getJumlah_meja() {
-        return Jumlah_meja;
-    }
-
-    public void setJumlah_meja(int Jumlah_meja) {
-        this.Jumlah_meja = Jumlah_meja;
-    }
-
-    public int getJumlah_jendela() {
-        return Jumlah_jendela;
-    }
-
-    public void setJumlah_jendela(int Jumlah_jendela) {
-        this.Jumlah_jendela = Jumlah_jendela;
-    }
-
-    public int getJumlah_pintu() {
-        return jumlah_pintu;
-    }
-
-    public void setJumlah_pintu(int jumlah_pintu) {
-        this.jumlah_pintu = jumlah_pintu;
+    public String getKondisi() {
+        return kondisi;
     }
     
-    double luas (){        
-        return getPanjang_ruang()*getLebar_ruang();
+
+    public String getUsername() {
+        return Username;
     }
-    void bentuk (){
-        if (getPanjang_ruang()!=luas()){
-            System.out.println("persegi panjang dan sesuai");
-        }else {
-            System.out.println("bukan persegi panjang");
-        }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
     }
-    double rasio(){
-        return luas()/getJumlah_kursi();        
+
+    public String getPasword() {
+        return pasword;
     }
-    void analisis (){
-        System.out.println("Jumlah pintu : "+getJumlah_pintu());
-        if(getJumlah_pintu()>=2){
-            System.out.println("sesuai");
-        } else {
-            System.out.println("tidak sesuai");
-        }
-        System.out.println("Jumlah jendela : "+getJumlah_jendela());
-        if(getJumlah_jendela()>=1){
-            System.out.println("sesuai");
+
+    public void setPasword(String pasword) {
+        this.pasword = pasword;
+    }
+    
+    public void setFasilitas (int kondisisteker, int kondisikabelLCD, int kondisilampu, int kondisikipasangin, int kondisiAC, int kondisiCCTV){
+        this.kondisisteker=kondisisteker;
+        this.kondisikabelLCD=kondisikabelLCD;
+        this.kondisilampu= kondisilampu;
+        this.kondisikipasangin=kondisikipasangin;
+        this.kondisiAC=kondisiAC;
+        this.kondisiCCTV=kondisiCCTV;
+    }
+
+    public int getKondisisteker() {
+        return kondisisteker;
+    }
+
+    public int getKondisikabelLCD() {
+        return kondisikabelLCD;
+    }
+
+    public int getKondisilampu() {
+        return kondisilampu;
+    }
+
+    public int getKondisikipasangin() {
+        return kondisikipasangin;
+    }
+
+    public int getKondisiAC() {
+        return kondisiAC;
+    }
+
+    public int getKondisiCCTV() {
+        return kondisiCCTV;
+    }
+    
+    public void setFasilitas (int posisikabelLCD, int posisilampu, int posisikipasangin, int posisiAC){
+        this.posisikabelLCD=posisikabelLCD;
+        this.posisilampu=posisilampu;
+        this.posisikipasangin=posisikipasangin;
+        this.posisiAC=posisiAC;
+    }
+
+    public int getPosisikabelLCD() {
+        return posisikabelLCD;
+    }
+
+    public int getPosisilampu() {
+        return posisilampu;
+    }
+
+    public int getPosisikipasangin() {
+        return posisikipasangin;
+    }
+
+    public int getPosisiAC() {
+        return posisiAC;
+    }
+    
+    public void setFasilitas(String username, String pasword){
+        this.Username=username;
+        this.pasword=pasword;
+    }
+    
+    public void setKondisiLingkungan(int lantai, int dinding, int atap, int pintu, int jendela){//overloading
+        this.atap=atap;
+        this.dinding=dinding;
+        this.lantai=lantai;
+        this.jendela=jendela;
+        this.pintu=pintu;
+    }
+
+    public int getLantai() {
+        return lantai;
+    }
+
+    public int getDinding() {
+        return dinding;
+    }
+
+    public int getAtap() {
+        return atap;
+    }
+
+    public int getPintu() {
+        return pintu;
+    }
+
+    public int getJendela() {
+        return jendela;
+    }
+    
+    public void setKondisiLingkungan(int sirkulasi_udara){
+        this.sirkulasi_udara=sirkulasi_udara;
+    }
+
+    public int getSirkulasi_udara() {
+        return sirkulasi_udara;
+    }
+    
+    public void setKondisiKenyamanan(int kebisingan, int bau, int kebocoran, int kerusakan, int keausan){
+        this.kebisingan=kebisingan;
+        this.bau=bau;
+        this.kebocoran=kebocoran;
+        this.kerusakan=kerusakan;
+        this.keausan=keausan;
+    }
+
+    public int getKebisingan() {
+        return kebisingan;
+    }
+
+    public int getBau() {
+        return bau;
+    }
+
+    public int getKebocoran() {
+        return kebocoran;
+    }
+
+    public int getKeausan() {
+        return keausan;
+    }
+
+    public int getKerusakan() {
+        return kerusakan;
+    }
+    
+    public void setKondisiKenyamanan(int kekokohan, int kunci_pintu, int kunci_jendela, int bahaya){
+        this.kekokohan=kekokohan;
+        this.kunci_pintu=kunci_pintu;
+        this.kunci_jendela=kunci_jendela;
+        this.bahaya=bahaya;
+    }
+
+    public int getKekokohan() {
+        return kekokohan;
+    }
+
+    public int getKunci_pintu() {
+        return kunci_pintu;
+    }
+
+    public int getKunci_jendela() {
+        return kunci_jendela;
+    }
+
+    public int getBahaya() {
+        return bahaya;
+    }
+
+    //abstract 
+    abstract void deklarasi1();
+    
+    String string1 (){
+        String pilih;
+        if(a==b){
+            pilih = "sesuai";
         }else{
-            System.out.println("tidak sesuai");
+            pilih="tidak sesuai";
         }
+        return pilih;
+    }
+    String string2 (){
+        String pilih;
+        if(a==b){
+            pilih = "sesuai";
+        }else{
+            pilih="tidak sesuai";
+        }
+        return pilih;
+    }
+    String string3 (){
+        String pilih;
+        if (a==b){
+            pilih="sesuai";
+        }else {
+            pilih="tidak sesuai";
+        }
+        return pilih;
+    }
+    String string4 (){
+        String pilih;
+        if(a==b){
+            pilih = "sesuai";
+        }else{
+            pilih="tidak sesuai";
+        }
+        return pilih;
+    }
+    String string5 (){
+        String pilih;
+        if(a==b){
+            pilih = "sesuai";
+        }else{
+            pilih="tidak sesuai";
+        }
+        return pilih;
+    }
+    String string6 (){
+        String pilih;
+        if(a==b){
+            pilih = "sesuai";
+        }else{
+            pilih="tidak sesuai";
+        }
+        return pilih;
+    }
+    String string7 (){
+        String pilih;
+        if(a==b){
+            pilih = "sesuai";
+        }else{
+            pilih="tidak sesuai";
+        }
+        return pilih;
     }
 }
