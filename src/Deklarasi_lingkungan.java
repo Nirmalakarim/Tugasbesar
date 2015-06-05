5115b31660bd64224a554693deaf4388bd752af0
Dini Saputri Yanianti <diniisaputrii@gmail.com>
2015-06-05 19:56:36 +0700

    deklarasi lingkungan (polimorfisme)

---
+++ src/Deklarasi_lingkungan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+
+/**
+ *
+ * @author NirmalaKarim
+ */
+public class Deklarasi_lingkungan {
+    Scanner scan = new Scanner(System.in);
+    void deklarasi (){
+        deklarasi_lingkungan (new Lingkungan("Tahap Kondisi Lingkungan"));
+        deklarasi_kebersihan (new Lingkungan("Tahap Kondisi Kebersihan"));
+    }    
+
+    public void deklarasi_lingkungan(Lingkungan test) {
+        test.deklarasi1();
+    }
+    void deklarasi_kebersihan(Lingkungan test) {        
+        test.deklarasi2();
+    }
+    
+}
