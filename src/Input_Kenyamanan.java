4d59bd56ddd8ca2894ac3bf83c74149560188751
Dini Saputri Yanianti <diniisaputrii@gmail.com>
2015-06-05 19:53:52 +0700

    Input kenyamanan (enkapsulasi)

---
+++ src/Input_Kenyamanan.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author NirmalaKarim
+ */
+public class Input_Kenyamanan implements Serializable{
+    private int pilih_kebisingan;
+    private int pilih_bau;
+    private int pilih_kebocoran;
+    private int pilih_kerusakan;
+    private int pilih_keausan;
+    private int pilih_kekokohan;
+    private int pilih_kuncipintu;
+    private int pilih_kuncijendela;
+    private int pilih_bahaya;
+
+    public int getPilih_kebisingan() {
+        return pilih_kebisingan;
+    }
+
+    public void setPilih_kebisingan(int pilih_kebisingan) {
+        this.pilih_kebisingan = pilih_kebisingan;
+    }
+
+    public int getPilih_bau() {
+        return pilih_bau;
+    }
+
+    public void setPilih_bau(int pilih_bau) {
+        this.pilih_bau = pilih_bau;
+    }
+
+    public int getPilih_kebocoran() {
+        return pilih_kebocoran;
+    }
+
+    public void setPilih_kebocoran(int pilih_kebocoran) {
+        this.pilih_kebocoran = pilih_kebocoran;
+    }
+
+    public int getPilih_kerusakan() {
+        return pilih_kerusakan;
+    }
+
+    public void setPilih_kerusakan(int pilih_kerusakan) {
+        this.pilih_kerusakan = pilih_kerusakan;
+    }
+
+    public int getPilih_keausan() {
+        return pilih_keausan;
+    }
+
+    public void setPilih_keausan(int pilih_keausan) {
+        this.pilih_keausan = pilih_keausan;
+    }
+
+    public int getPilih_kekokohan() {
+        return pilih_kekokohan;
+    }
+
+    public void setPilih_kekokohan(int pilih_kekokohan) {
+        this.pilih_kekokohan = pilih_kekokohan;
+    }
+
+    public int getPilih_kuncipintu() {
+        return pilih_kuncipintu;
+    }
+
+    public void setPilih_kuncipintu(int pilih_kuncipintu) {
+        this.pilih_kuncipintu = pilih_kuncipintu;
+    }
+
+    public int getPilih_kuncijendela() {
+        return pilih_kuncijendela;
+    }
+
+    public void setPilih_kuncijendela(int pilih_kuncijendela) {
+        this.pilih_kuncijendela = pilih_kuncijendela;
+    }
+
+    public int getPilih_bahaya() {
+        return pilih_bahaya;
+    }
+
+    public void setPilih_bahaya(int pilih_bahaya) {
+        this.pilih_bahaya = pilih_bahaya;
+    }
+
+    
+}
