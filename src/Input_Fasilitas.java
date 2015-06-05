461006c864f9d3090927ae3a6774b00454e83798
Dini Saputri Yanianti <diniisaputrii@gmail.com>
2015-06-05 19:53:05 +0700

    Input Fasilitas (enkapsulasi)

---
+++ src/Input_Fasilitas.java
@@ -0,0 +1,197 @@
+
+import java.io.Serializable;
+
+
+public class Input_Fasilitas implements Serializable{
+    private int pilih_kondisisteker;
+    private int pilih_kondisilampu;
+    private int pilih_kondisikabel;
+    private int pilih_kondisikipas;
+    private int pilih_kondisiac;
+    private int pilih_kondisicctv;
+    private int pilih_posisikabel;
+    private int pilih_posisilampu;
+    private int pilih_posisikipas;
+    private int pilih_posisiac;
+    private String pilih_username;
+    private String pilih_pasword;
+    private int Jumlahsteker;
+    private int jumlahkabelLCD;
+    private int jumlahlampu;
+    private int jumlahkipasangin;
+    private int jumlahAC;
+    private int jumlahCCTV;
+    private int posisisteker;
+    private int posisiCCTV;
+    private int SSID;
+    
+    public int getPilih_kondisisteker() {
+        return pilih_kondisisteker;
+    }
+
+    public void setPilih_kondisisteker(int pilih_kondisisteker) {
+        this.pilih_kondisisteker = pilih_kondisisteker;
+    }
+
+    public int getPilih_kondisilampu() {
+        return pilih_kondisilampu;
+    }
+
+    public void setPilih_kondisilampu(int pilih_kondisilampu) {
+        this.pilih_kondisilampu = pilih_kondisilampu;
+    }
+
+    public int getPilih_kondisikabel() {
+        return pilih_kondisikabel;
+    }
+
+    public void setPilih_kondisikabel(int pilih_kondisikabel) {
+        this.pilih_kondisikabel = pilih_kondisikabel;
+    }
+
+    public int getPilih_kondisikipas() {
+        return pilih_kondisikipas;
+    }
+
+    public void setPilih_kondisikipas(int pilih_kondisikipas) {
+        this.pilih_kondisikipas = pilih_kondisikipas;
+    }
+
+    public int getPilih_kondisiac() {
+        return pilih_kondisiac;
+    }
+
+    public void setPilih_kondisiac(int pilih_kondisiac) {
+        this.pilih_kondisiac = pilih_kondisiac;
+    }
+
+    public int getPilih_kondisicctv() {
+        return pilih_kondisicctv;
+    }
+
+    public void setPilih_kondisicctv(int pilih_kondisicctv) {
+        this.pilih_kondisicctv = pilih_kondisicctv;
+    }
+
+    public int getPilih_posisikabel() {
+        return pilih_posisikabel;
+    }
+
+    public void setPilih_posisikabel(int pilih_posisikabel) {
+        this.pilih_posisikabel = pilih_posisikabel;
+    }
+
+    public int getPilih_posisilampu() {
+        return pilih_posisilampu;
+    }
+
+    public void setPilih_posisilampu(int pilih_posisilampu) {
+        this.pilih_posisilampu = pilih_posisilampu;
+    }
+
+    public int getPilih_posisikipas() {
+        return pilih_posisikipas;
+    }
+
+    public void setPilih_posisikipas(int pilih_posisikipas) {
+        this.pilih_posisikipas = pilih_posisikipas;
+    }
+
+    public int getPilih_posisiac() {
+        return pilih_posisiac;
+    }
+
+    public void setPilih_posisiac(int pilih_posisiac) {
+        this.pilih_posisiac = pilih_posisiac;
+    }
+
+    public String getPilih_username() {
+        return pilih_username;
+    }
+
+    public void setPilih_username(String pilih_username) {
+        this.pilih_username = pilih_username;
+    }
+
+    public String getPilih_pasword() {
+        return pilih_pasword;
+    }
+
+    public void setPilih_pasword(String pilih_pasword) {
+        this.pilih_pasword = pilih_pasword;
+    }
+
+    public int getJumlahsteker() {
+        return Jumlahsteker;
+    }
+
+    public void setJumlahsteker(int Jumlahsteker) {
+        this.Jumlahsteker = Jumlahsteker;
+    }
+
+    public int getJumlahkabelLCD() {
+        return jumlahkabelLCD;
+    }
+
+    public void setJumlahkabelLCD(int jumlahkabelLCD) {
+        this.jumlahkabelLCD = jumlahkabelLCD;
+    }
+
+    public int getJumlahlampu() {
+        return jumlahlampu;
+    }
+
+    public void setJumlahlampu(int jumlahlampu) {
+        this.jumlahlampu = jumlahlampu;
+    }
+
+    public int getJumlahkipasangin() {
+        return jumlahkipasangin;
+    }
+
+    public void setJumlahkipasangin(int jumlahkipasangin) {
+        this.jumlahkipasangin = jumlahkipasangin;
+    }
+
+    public int getJumlahAC() {
+        return jumlahAC;
+    }
+
+    public void setJumlahAC(int jumlahAC) {
+        this.jumlahAC = jumlahAC;
+    }
+
+    public int getJumlahCCTV() {
+        return jumlahCCTV;
+    }
+
+    public void setJumlahCCTV(int jumlahCCTV) {
+        this.jumlahCCTV = jumlahCCTV;
+    }
+
+    public int getPosisisteker() {
+        return posisisteker;
+    }
+
+    public void setPosisisteker(int posisisteker) {
+        this.posisisteker = posisisteker;
+    }
+
+    public int getPosisiCCTV() {
+        return posisiCCTV;
+    }
+
+    public void setPosisiCCTV(int posisiCCTV) {
+        this.posisiCCTV = posisiCCTV;
+    }
+
+    public int getSSID() {
+        return SSID;
+    }
+
+    public void setSSID(int SSID) {
+        this.SSID = SSID;
+    }
+    
+    
+}
