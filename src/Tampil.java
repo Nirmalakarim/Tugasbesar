d9fe0080690e7c464d8e7823d2aa52e6dd092546
Dini Saputri Yanianti <diniisaputrii@gmail.com>
2015-06-05 19:57:16 +0700

    tampil (main)

---
+++ src/Tampil.java
@@ -0,0 +1,12 @@
+public class Tampil {
+    public static void main(String[] args) {
+        Deklarasi_lingkungan test1 = new Deklarasi_lingkungan();
+        Deklarasi_Kenyamanan test2 = new Deklarasi_Kenyamanan();
+        Deklarasi_kondisi test3 = new Deklarasi_kondisi(); 
+        Deklarasi_fasilitas test4 = new Deklarasi_fasilitas();
+        test3.deklarasi();
+        test4.deklarasi();
+        test1.deklarasi();  
+        test2.deklarasi();
+    }
+}
