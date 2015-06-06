d382d5c184f74fec423c3e359043f04d37fbee46
NirmalaKarim <NirmalaKarim@toshiba>
2015-06-06 07:36:25 +0700

    Tampil (main)

--- src/Tampil.java
+++ src/Tampil.java
@@ -2,6 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
 /**
  *
  * @author NirmalaKarim
@@ -12,9 +14,39 @@ public class Tampil {
         Deklarasi_Kenyamanan test2 = new Deklarasi_Kenyamanan();
         Deklarasi_kondisi test3 = new Deklarasi_kondisi(); 
         Deklarasi_Fasilitas test4 = new Deklarasi_Fasilitas();
-        test1.deklarasi();
         test3.deklarasi();
-        test4.deklarasi();  
+        test4.deklarasi();
+        test1.deklarasi();  
         test2.deklarasi();
+        
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login_gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login_gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login_gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login_gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login_gui().setVisible(true);
+            }
+        });
     }
 }
