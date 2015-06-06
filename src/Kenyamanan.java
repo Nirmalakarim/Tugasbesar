ec9cd247b676b5ac514c545593031cc7d1cde961
NirmalaKarim <NirmalaKarim@toshiba>
2015-06-06 07:31:56 +0700

    Kenyamanan (saving objek)

--- src/Kenyamanan.java
+++ src/Kenyamanan.java
@@ -2,99 +2,21 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author NirmalaKarim
  */
 public class Kenyamanan extends Ruangkelas implements Deklarasi{
-    private int pilih_kebisingan;
-    private int pilih_bau;
-    private int pilih_kebocoran;
-    private int pilih_kerusakan;
-    private int pilih_keausan;
-    private int pilih_kekokohan;
-    private int pilih_kuncipintu;
-    private int pilih_kuncijendela;
-    private int pilih_bahaya;
-
+    Input_kenyamanan test = new Input_kenyamanan();
     public Kenyamanan(String kondisi) {
         super(kondisi);//konstraktor
-    }
-
-    //enkapsulasi
-    public int getPilih_kebisingan() {
-        return pilih_kebisingan;
-    }
-
-    public void setPilih_kebisingan(int pilih_kebisingan) {
-        this.pilih_kebisingan = pilih_kebisingan;
-    }
-
-    public int getPilih_bau() {
-        return pilih_bau;
-    }
-
-    public void setPilih_bau(int pilih_bau) {
-        this.pilih_bau = pilih_bau;
-    }
-
-    public int getPilih_kebocoran() {
-        return pilih_kebocoran;
-    }
-
-    public void setPilih_kebocoran(int pilih_kebocoran) {
-        this.pilih_kebocoran = pilih_kebocoran;
-    }
-
-    public int getPilih_kerusakan() {
-        return pilih_kerusakan;
-    }
-
-    public void setPilih_kerusakan(int pilih_kerusakan) {
-        this.pilih_kerusakan = pilih_kerusakan;
-    }
-
-    public int getPilih_keausan() {
-        return pilih_keausan;
-    }
-
-    public void setPilih_keausan(int pilih_keausan) {
-        this.pilih_keausan = pilih_keausan;
-    }
-
-    public int getPilih_kekokohan() {
-        return pilih_kekokohan;
-    }
-
-    public void setPilih_kekokohan(int pilih_kekokohan) {
-        this.pilih_kekokohan = pilih_kekokohan;
-    }
-
-    public int getPilih_kuncipintu() {
-        return pilih_kuncipintu;
-    }
-
-    public void setPilih_kuncipintu(int pilih_kuncipintu) {
-        this.pilih_kuncipintu = pilih_kuncipintu;
-    }
-
-    public int getPilih_kuncijendela() {
-        return pilih_kuncijendela;
-    }
-
-    public void setPilih_kuncijendela(int pilih_kuncijendela) {
-        this.pilih_kuncijendela = pilih_kuncijendela;
-    }
-
-    public int getPilih_bahaya() {
-        return pilih_bahaya;
-    }
-
-    public void setPilih_bahaya(int pilih_bahaya) {
-        this.pilih_bahaya = pilih_bahaya;
     }
 
     @Override
@@ -105,48 +27,59 @@ public class Kenyamanan extends Ruangkelas implements Deklarasi{
         System.out.println("1. Bising");
         System.out.println("2. Tidak Bising");
         System.out.print("Jawab : ");
-        setPilih_kebisingan(scan.nextInt());
+        test.setPilih_kebisingan(scan.nextInt());
         System.out.println("Kebisingan :"+string1());
         
         System.out.println("B. Apakah di kelas bau");
         System.out.println("1. Bau");
         System.out.println("2. Tidak Bau");
         System.out.print("Jawab : ");
-        setPilih_bau(scan.nextInt());
+        test.setPilih_bau(scan.nextInt());
         System.out.println("Bau :"+string2());
                
         System.out.println("C. Apakah di kelas ada yang bocor");
         System.out.println("1. Ada ");
         System.out.println("2. Tidak Ada");
         System.out.print("Jawab : ");
-        setPilih_kebocoran(scan.nextInt());
+        test.setPilih_kebocoran(scan.nextInt());
         System.out.println("Kebocoran :"+string3());
         
         System.out.println("D. Apakah di kelas ada aus");
         System.out.println("1. Ada ");
         System.out.println("2. Tidak Ada");
         System.out.print("Jawab : ");
-        setPilih_keausan(scan.nextInt());
+        test.setPilih_keausan(scan.nextInt());
         System.out.println("Keausan :"+string4());
         
         System.out.println("E. Apakah di kelas ada yang rusak");
         System.out.println("1. Ada ");
         System.out.println("2. Tidak Ada ");
         System.out.print("Jawab : ");
-        setPilih_kerusakan(scan.nextInt());
+        test.setPilih_kerusakan(scan.nextInt());
         System.out.println("Kerusakan :"+string5());
+        System.out.println("");
+        try(FileOutputStream fs = new FileOutputStream("data.txt")){
+                ObjectOutputStream ob = new ObjectOutputStream(fs);
+                ob.writeObject(test);
+                ob.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
         
-        try(FileOutputStream fs = new FileOutputStream("penyimpanan.txt")){
-            ObjectOutputStream os = new ObjectOutputStream(fs);
-            os.writeObject(getPilih_kebisingan());
-            os.writeObject(getPilih_bau());
-            os.writeObject(getPilih_kebocoran());
-            os.writeObject(getPilih_keausan());
-            os.writeObject(getPilih_kerusakan());
-            os.close();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
+            try (FileInputStream fs = new FileInputStream("data.txt")){
+                ObjectInputStream ib = new ObjectInputStream(fs);
+                Input_kenyamanan it = (Input_kenyamanan) ib.readObject();
+                System.out.println("Kebisingan "+it.getPilih_kebisingan());
+                System.out.println("Bau "+it.getPilih_bau());
+                System.out.println("Kebocoran "+it.getPilih_kebocoran());
+                System.out.println("Kerusakan "+it.getPilih_kerusakan());
+                System.out.println("Keausan "+it.getPilih_keausan());
+                ib.close();
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(Kenyamanan.class.getName()).log(Level.SEVERE, null, ex);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
     }
 
     @Override
@@ -157,92 +90,103 @@ public class Kenyamanan extends Ruangkelas implements Deklarasi{
         System.out.println("1. Masih Kokoh");
         System.out.println("2. Tidak Kokoh");
         System.out.print("Jawab : ");
-        setPilih_kekokohan(scan.nextInt());
+        test.setPilih_kekokohan(scan.nextInt());
         System.out.println("Kekokohan :"+string6());
         
         System.out.println("B. Bagaimana keaadaan kunci pintu");
         System.out.println("1. Ada");
         System.out.println("2. Tidak ada");
         System.out.print("Jawab : ");
-        setPilih_kuncipintu(scan.nextInt());
+        test.setPilih_kuncipintu(scan.nextInt());
         
         System.out.println("C. Bagaimana keaadaan kunci jendela");
         System.out.println("1. Ada");
         System.out.println("2. Tidak ada");
         System.out.print("Jawab : ");
-        setPilih_kuncijendela(scan.nextInt());
+        test.setPilih_kuncijendela(scan.nextInt());
         System.out.println("Analisis kunci pintu dan jendela :"+kunci());
         
         System.out.println("D. Apakah di kelas ada Bahaya");
         System.out.println("1. Aman");
         System.out.println("2. Tidak Aman");
         System.out.print("Jawab : ");
-        setPilih_bahaya(scan.nextInt());
+        test.setPilih_bahaya(scan.nextInt());
         System.out.println("Keamanan :"+string7());
+        System.out.println("");
+        try(FileOutputStream fs = new FileOutputStream("data.txt")){
+                ObjectOutputStream ob = new ObjectOutputStream(fs);
+                ob.writeObject(test);
+                ob.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
         
-        try(FileOutputStream fs = new FileOutputStream("penyimpanan.txt")){
-            ObjectOutputStream os = new ObjectOutputStream(fs);
-            os.writeObject(getPilih_kekokohan());
-            os.writeObject(getPilih_kuncipintu());
-            os.writeObject(getPilih_kuncijendela());
-            os.writeObject(getPilih_bahaya());
-            os.close();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
+            try (FileInputStream fs = new FileInputStream("data.txt")){
+                ObjectInputStream ib = new ObjectInputStream(fs);
+                Input_kenyamanan it = (Input_kenyamanan) ib.readObject();
+                System.out.println("Kekokohan "+it.getPilih_kekokohan());
+                System.out.println("Kunci pintu "+it.getPilih_kuncipintu());
+                System.out.println("Kunci jendela "+it.getPilih_kuncijendela());
+                System.out.println("Bahaya "+it.getPilih_bahaya());
+                ib.close();
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(Kenyamanan.class.getName()).log(Level.SEVERE, null, ex);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
     }
 
     @Override
     String string1() {
-        a=getPilih_kebisingan();
+        a=test.getPilih_kebisingan();
         b=getKebisingan();
         return super.string1();
     }
 
     @Override
     String string2() {
-        a=getPilih_bau();
+        a=test.getPilih_bau();
         b=getBau();
         return super.string2();
     }
 
     @Override
     String string3() {
-        a=getPilih_kebocoran();
+        a=test.getPilih_kebocoran();
         b=getKebocoran();
         return super.string3();
     }
 
     @Override
     String string4() {
-        a=getPilih_keausan();
+        a=test.getPilih_keausan();
         b=getKeausan();
         return super.string4();
     }
 
     @Override
     String string5() {
-        a=getPilih_kerusakan();
+        a=test.getPilih_kerusakan();
         b=getKerusakan();
         return super.string5();
     }
 
     @Override
     String string6() {
-        a=getPilih_kekokohan();
+        a=test.getPilih_kekokohan();
         b=getKekokohan();
         return super.string6();
     }
 
     @Override
     String string7() {
-        a=getPilih_bahaya();
+        a=test.getPilih_bahaya();
         b=getBahaya();
         return super.string7();
     }
     String kunci(){
         String pilih;
-        if(getPilih_kuncipintu()==getKunci_pintu() && getPilih_kuncijendela()==getKunci_jendela()){
+        if(test.getPilih_kuncipintu()==getKunci_pintu() &&test.getPilih_kuncijendela()==getKunci_jendela()){
             pilih="sesuai";
         } else{
             pilih="tidak sesuai";
