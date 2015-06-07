7bf0f8370c6385e73f551072ac247de0f2bb8ea0
Dini Saputri Yanianti <diniisaputrii@gmail.com>
2015-06-07 13:37:45 +0700

    Update Input_lingkungan.java

--- src/Input_lingkungan.java
+++ src/Input_lingkungan.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.io.Serializable;
 
-/**
- *
- * @author NirmalaKarim
- */
 public class Input_lingkungan implements Serializable{
     private int pilih_atap;
     private int pilih_dinding;
